5a69a60836336705a083e448c2f79da879ff5411
Fredrik Larsen <fredrik.larsen@usit.uio.no>
2016-09-28 15:31:40 +0200

    Adds custom error handler for all routes

--- pofh/api/__init__.py
+++ pofh/api/__init__.py
@@ -1,8 +1,10 @@
 # encoding: utf-8
 """ Assembled API. """
-from __future__ import unicode_literals
+from __future__ import unicode_literals, absolute_import
 
 from flask import g, jsonify
+import werkzeug.exceptions as _exc
+from blinker import signal
 from .. import auth
 
 from . import usernames
@@ -10,10 +12,43 @@ from . import password
 from . import sms_password
 
 
+api_error = signal('api.error')
+
+
+def handle_api_error(error):
+    """ Handle errors.
+
+    This handler attempts to jsonify uncaught exceptions.
+    """
+    api_error.send(type(error), exc=error)
+
+    if isinstance(error, _exc.HTTPException):
+        r = error.get_response()
+        response = jsonify(
+            message=error.description,
+            error=error.name)
+        response.www_authenticate.realm = r.www_authenticate.realm
+        response.www_authenticate.type = r.www_authenticate.type
+    else:
+        response = jsonify(
+            message=str(error),
+            error=str("unknown")
+        )
+
+    response.status_code = getattr(error, 'code', 500)
+    return response
+
+
 def init_app(app):
+    """ Bootstrap the API. """
     app.register_blueprint(usernames.API)
     app.register_blueprint(password.API)
     app.register_blueprint(sms_password.API)
+
+    app.errorhandler(Exception)(handle_api_error)
+    # Workaround for https://github.com/pallets/flask/issues/941
+    for code in _exc.default_exceptions:
+        app.errorhandler(code)(handle_api_error)
 
     @app.route('/refresh', methods=['POST', ])
     @auth.require_jwt()

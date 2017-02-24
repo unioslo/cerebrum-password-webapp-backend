f121241f2a31a0230257196337089deb236b9eb5
Jo Sama <jo.sama@usit.uio.no>
2017-02-24 13:04:31 +0100

    Refer to config after it has been inited.

--- pofh/__init__.py
+++ pofh/__init__.py
@@ -214,12 +214,13 @@ class WsgiApp(object):
             static_folder=None,
             instance_relative_config=True)
 
+        init_config(app, config)
+
         if app.config.get('NUMBER_OF_PROXIES', None):
             app.wsgi_app = ProxyFix(app.wsgi_app,
                                     num_proxies=app.config.get(
                                         'NUMBER_OF_PROXIES'))
 
-        init_config(app, config)
         init_logging(app)
 
         # setup CORS support
